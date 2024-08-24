d535322de86a02573ed94b458c2966673d0af0ff
resist15 <sourav@aospa.co>
2024-08-24 13:28:31 +0530

    sm8150-common: Adapt to PixelOS
    Change-Id: Ib047751f553a4caec34b991f124567700bea91b6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -160,9 +160,6 @@ endif # !TARGET_IS_LEGACY
 BOARD_SUPER_PARTITION_GROUPS := qti_dynamic_partitions
 BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := $(ALL_PARTITIONS)
 
-# Partitions - reserved size
--include vendor/lineage/config/BoardConfigReservedSize.mk
-
 # Platform
 BOARD_VENDOR := xiaomi
 BOARD_USES_QCOM_HARDWARE := true
@@ -246,7 +243,7 @@ endif
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/aosp/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 ifeq ($(TARGET_HAS_FM),true)
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_fm.xml
